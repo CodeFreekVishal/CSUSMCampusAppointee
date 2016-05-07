bc4b85b93596e93df82825c52f3339cddf0143f9
Vishal Arora <astrovishy@gmail.com>
2016-05-08 01:30:41 +0530

    m2

--- app/src/main/java/com/example/jal/csusmcampusappointee/LoginActivity.java
+++ app/src/main/java/com/example/jal/csusmcampusappointee/LoginActivity.java
@@ -1,47 +1,53 @@
 package com.example.jal.csusmcampusappointee;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+    AutoCompleteTextView email;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        Button sendbutton=(Button)findViewById(R.id.email_sign_in_button);
+        email = (AutoCompleteTextView) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        Button sendbutton = (Button) findViewById(R.id.email_sign_in_button);
 
         sendbutton.setOnClickListener(
                 new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Homepage.class);
-                startActivity(intent);
-            }
-        });
-        };
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onClick(View view) {
+                        String emailText = email.getText().toString();
+                        String pwdText = password.getText().toString();
+
+                        if (emailText.isEmpty() || pwdText.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Please enter proper login credentials", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Intent intent = new Intent(getApplicationContext(), Homepage.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    ;
 
 
 }
 
-//sendbutton.setOnClickListener(
-  //      new View.OnClickListener() {
-//@Override
-//public void onClick(View view) {
-  //      Intent intent = new Intent(getApplicationContext(), Homepage.class);
-    //    startActivity(intent);
-      //  }
-        //});
 
 
