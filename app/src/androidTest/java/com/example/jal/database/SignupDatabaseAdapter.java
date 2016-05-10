a9dca435446d9d1296661d5706dbe888ee7fb0c4
Vishal Arora <astrovishy@gmail.com>
2016-05-10 21:25:42 +0530

    m10 Db in progress

--- app/src/androidTest/java/com/example/jal/database/SignupDatabaseAdapter.java
+++ app/src/androidTest/java/com/example/jal/database/SignupDatabaseAdapter.java
@@ -1,6 +1,8 @@
 package com.example.jal.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -30,7 +32,7 @@ public class SignupDatabaseAdapter extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table details" + "(id integer primary key, fullname text, emailid text, password text, department text, contact text)");
+        db.execSQL("create table details" + "(id integer primary key auto_increment, fullname text, emailid text, password text, department text, contact text)");
     }
 
     @Override
@@ -38,6 +40,56 @@ public class SignupDatabaseAdapter extends SQLiteOpenHelper {
         Log.w("TaskDbAdapter", "Upgrading database from" + oldVersion +  "to" + newVersion + ",which will destroy all data...");
         db.execSQL("DROP TABLE IF EXISTS"+table_name);
         onCreate(db);
+    }
+
+    public boolean insertRecords(String fullname, String emailid, String password, String department, String contact){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(col_fullname, fullname);
+        contentValues.put(col_email, emailid);
+        contentValues.put(col_password, password);
+        contentValues.put(col_deparment, department);
+        contentValues.put(col_contact, contact);
+
+        db.insert(table_name, null,  contentValues);
+        return true;
+    }
+
+
+    public boolean updateRecords(Integer id, String fullname, String emailid, String password, String department, String contact){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(col_fullname, fullname);
+        contentValues.put(col_email, emailid);
+        contentValues.put(col_password, password);
+        contentValues.put(col_deparment, department);
+        contentValues.put(col_contact, contact);
+
+        db.update(table_name, contentValues, col_id+"=?",new String[] { Integer.toString(id) });
+        return true;
+    }
+
+    public Cursor getRecord(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "SELECT * FROM " + table_name + " WHERE " +
+                col_id + "=?", new String[] { Integer.toString(id) } );
+        return res;
+    }
+
+    public Cursor getAllRecords() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "SELECT * FROM " + table_name, null );
+        return res;
+    }
+
+
+    public Integer deleteRecord(Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(table_name,
+                col_id + " = ? ",
+                new String[] { Integer.toString(id) });
     }
 
 
