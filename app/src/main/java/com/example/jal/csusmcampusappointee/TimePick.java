267b487b98000aea33f7bafe052f4b64aa82a5a7
Vishal Arora <astrovishy@gmail.com>
2016-05-17 19:34:14 +0530

    m18

--- app/src/main/java/com/example/jal/csusmcampusappointee/TimePick.java
+++ app/src/main/java/com/example/jal/csusmcampusappointee/TimePick.java
@@ -47,7 +47,7 @@ public class TimePick extends Activity {
 
                 Intent i = new Intent(Intent.ACTION_SEND);
                 i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"astrovishy@gmail.com"});
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"achilles.jal@gmail.com"});
                 i.putExtra(Intent.EXTRA_SUBJECT, subject);
                 i.putExtra(Intent.EXTRA_TEXT, "Respected Professor, \n \n \t San Marcos student "+name+ " would like to book an appointment with you on "+
                     month+"/"+day+"/"+year+" at "+currenthour+":"+currentmin+
